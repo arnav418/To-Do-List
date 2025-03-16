1a1dec1365f4ccfa2a8c38a531fcfd1b718ef91e
arnav0418 <arnavpriya05@gmail.com>
2025-03-16 12:05:41 +0530

    Some Changes

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 const button = document.getElementById('text-btn');
 let textField = document.getElementById('text-inp');
 let listItems = document.querySelector('ol');
+let head = document.querySelector('h1');
 
 button.addEventListener("click", function() {
     if (textField.value === "") {
+        head.textContent = "Put Some task ⚠️";
         alert("Add Task in Input");
-    } else {
+        
+    }
+    
+    else {
         let storage = textField.value;
         console.log(storage);
         let dataStorage = document.createElement("li"); 
         dataStorage.textContent = storage; 
         listItems.appendChild(dataStorage); 
         textField.value = ""; 
+        dataStorage.addEventListener('click',function(){
+        setTimeout(listItems.removeChild(dataStorage),500);
+        });
     }
+    
 });
+
+
 
